eb8b9ce869e4aed0c664023afd6c694610dbfa17
Andre Hartmann <aha_1980@gmx.de>
2018-11-10 16:51:05 +0100

    Pyramid V2

--- pyramid.py
+++ pyramid.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
-pyramid = """
-   *
-  ***
- *****
-*******
-"""
-
-print(pyramid)
+spaces = 3
+stars  = 1
+for line in range(0, 4):
+	for i in range(0, spaces):
+		print(" ", end = "")
+	for i in range(0, stars):
+		print("*", end = "")
+	print("")
+	spaces -= 1
+	stars  += 2
