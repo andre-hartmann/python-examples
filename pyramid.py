b577a690d9007af5986f911a01e5d875b3961631
Andre Hartmann <aha_1980@gmx.de>
2018-11-10 16:53:39 +0100

    Pyramid V3

--- pyramid.py
+++ pyramid.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-spaces = 3
+lines = 20
+
+spaces = lines - 1
 stars  = 1
-for line in range(0, 4):
+for line in range(0, lines):
 	for i in range(0, spaces):
 		print(" ", end = "")
 	for i in range(0, stars):
